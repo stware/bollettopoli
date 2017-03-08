1521f9d59aecf2bad4753531d8eba2dc3f62ef5e
stware <stef.turrini@gmail.com>
2017-03-08 21:23:13 +0100

    First navigable site

--- webapp/app.module.js
+++ webapp/app.module.js
@@ -2,44 +2,41 @@
  * Created by stefano on 05/03/17.
  */
 // public/core.js
-var scotchTodo = angular.module('scotchTodo', []);
+var indexModule = angular.module('index', ['ui.router']);
 
-function mainController($scope, $http) {
+indexModule.controller('mainController',['$scope','$http',function($scope, $http) {
     $scope.formData = {};
 
     // when landing on the page, get all todos and show them
-    $http.get('../api/contacts')
-        .success(function(data) {
-            $scope.todos = data.rows;
-            console.log(data);
-        })
-        .error(function(data) {
-            console.log('Error: ' + data);
+    $http.get('../api/contacts').then(function(response) {
+            $scope.todos = response.data.rows;
+            console.log(response.data.rows);
+        },function(response) {
+            console.log('Error: ' + response);
         });
 
     // when submitting the add form, send the text to the node API
     $scope.createTodo = function() {
         $http.post('../api/contacts', $scope.formData)
-            .success(function(data) {
+            .then(function(response) {
                 $scope.formData = {}; // clear the form so our user is ready to enter another
-                $scope.todos = data.rows;
-                console.log(data.rows);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
+                $scope.todos = response.data.rows;
+                console.log(response.data.rows);
+            },function(response) {
+                console.log('Error: ' + response);
             });
     };
 
     // delete a todo after checking it
     $scope.deleteTodo = function(id) {
         $http.delete('../api/contacts/' + id)
-            .success(function(data) {
-                $scope.todos = data.rows;
-                console.log(data);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
+            .then(function(response) {
+                $scope.todos = response.data.rows;
+                console.log(response.data.rows);
+            },function(response) {
+                console.log('Error: ' + response);
             });
     };
 
-}
+}]);
+
--- webapp/app.route.js
+++ webapp/app.route.js
@@ -1,3 +1,21 @@
 /**
  * Created by stefano on 05/03/17.
  */
+angular.module('index').config(function($stateProvider, $urlRouterProvider) {
+    //
+    // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise("/home");
+    //
+    // Now set up the states
+    $stateProvider
+        .state('home', {
+            url: "/home",
+            templateUrl: "/home/home.html"
+        }).state('todo', {
+        url: "/todo",
+        templateUrl: "/todo/todo.html"
+        }).state('about', {
+            url: "/about",
+            templateUrl: "/about/about.html"
+        });
+});
